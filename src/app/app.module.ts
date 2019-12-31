4e800b9a5c4a16bc919df03ca1e613ceaf365bc1
Mohammed Rawoof <inform.smohdr88@gmail.com>
2019-12-30 20:28:45 -0500

    adding mulitple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopNavComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ AppComponent, TopNavComponent ],
+  imports: [ NgbModule, BrowserModule, AppRoutingModule ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent, TopNavComponent ]
 })
 export class AppModule { }
